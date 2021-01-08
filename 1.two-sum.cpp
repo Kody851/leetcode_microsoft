fb2b7555668895ec149d806b6ef6dff5e7b57a65
kody <2270493988@qq.com>
2021-01-08 13:38:54 +0800

    1

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,7 +8,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        11
+        vector<int>res;
+        if(nums.empty()){
+            return res;
+        }
+        int n = nums.size();
+        unordered_map<int,int>hash;
+        for(int i=0;i<n;i++){
+            if(hash.find(target-nums[i])!=hash.end()){
+                res.push_back(i);
+                res.push_back(hash[target-nums[i]]);
+            }
+            hash[nums[i]] = i;
+        }
+        return res;
     }
 };
 // @lc code=end
